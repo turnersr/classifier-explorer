4ca40bcdd8e34d15eae25ac6f42f92227d959f27
rturner <turnersr@uchicago.edu>
2014-08-17 16:11:27 -0500

    download scripts should all work now

--- data_ingestion_and_creation/download_scripts/test_images.sh
+++ data_ingestion_and_creation/download_scripts/test_images.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 DL=../../datasets/test_images/
-wget -o 500_images.tar.gz --no-check-certificate https://www.dropbox.com/s/oz8uv312rvlqx0a/500_images.tar.gz
+wget -O 500_images.tar.gz --no-check-certificate https://www.dropbox.com/s/s7k3uz782fwgcen/500_images.tar.gz
 mkdir -p $DL
 tar -vxzf 500_images.tar.gz -C $DL
 rm 500_images.tar.gz
