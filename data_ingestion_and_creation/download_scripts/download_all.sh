44f975b62f0eb05ac6f445ee269736d60656e9dc
OpsWorks user rturner <rturner@nfp2.localdomain>
2014-08-18 06:58:06 +0000

    model result reading script and download

--- data_ingestion_and_creation/download_scripts/download_all.sh
+++ data_ingestion_and_creation/download_scripts/download_all.sh
@@ -6,6 +6,6 @@ bash test_images.sh
 bash newyork_streetview_queries.sh
 bash zipcode_to_latlong.sh
 bash zillow.sh
-
+bash model_results.sh
 
 
--- data_ingestion_and_creation/download_scripts/zipcode_to_latlong.sh
+++ data_ingestion_and_creation/download_scripts/zipcode_to_latlong.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 DL=../../datasets/zipcode_latlong/
-wget -o zipcode_latlong.tar.gz --no-check-certificate https://www.dropbox.com/s/zy1zpwbbrir3emu/zipcode_latlong.tar.gz
+wget -O zipcode_latlong.tar.gz --no-check-certificate https://www.dropbox.com/s/zy1zpwbbrir3emu/zipcode_latlong.tar.gz
 mkdir -p $DL
 tar -vxzf zipcode_latlong.tar.gz -C $DL
 rm zipcode_latlong.tar.gz
