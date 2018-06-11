b8a4f3dcd50b00a636028ceb3eb84b4fad6d293c
turnersr <turnersr@users.noreply.github.com>
2018-06-10 22:14:58 -0400

    Update scikit_learn_predict.py

--- scikit_learn_predict.py
+++ scikit_learn_predict.py
@@ -15,13 +15,6 @@ classifier_stack.initialize_classifiers(model_setup)
 X = np.load(model_setup["X"])
 y = np.load(model_setup["y"])
 
-#print positive_feature
-#print negative_feature
-#X,y = design_matrix_with_target(positive_feature, negative_feature) 
-
-print(X.shape)
-print(y.shape)
-
 final_results = []
 total_clf = len(classifier_stack.classifiers_to_fit)
 print("[x] total classifiers %d" % total_clf)
@@ -39,5 +32,4 @@ number_jobs = -1
 
 classifiers_to_fit = classifier_stack.classifiers_to_fit
 scores = train_save_test_multiple_classifiers(location_to_save_models, number_jobs, classifiers_to_fit, X_train, y_train, X_test, y_test)
-#print(scores)
 np.savez(location_to_save_model_metrics+"/results", scores,X_train,y_train,X_test,y_test)
